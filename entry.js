96cb09a31b55c81fa78f3fcb106ab0efea978575
mreilaender <manuel.reilaender@gmail.com>
2017-11-23 11:41:46 +0100

    I really tried hard

---
+++ entry.js
@@ -0,0 +1,3 @@
+import { styles } from './src/test';
+
+console.log(styles);
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    context: __dirname,
+    entry: './entry.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            }
+        ]
+    }
+};
